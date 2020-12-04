61882c73b1344222fb9ca3ac9582ccb6371971ad
Sonu Kumar <sonunitw12@gmail.com>
2020-12-04 22:24:41 +0530

    disable test in ci env

--- rqueue-spring-boot-starter/src/test/java/com/github/sonus21/rqueue/spring/boot/tests/integration/PeriodicMessageTest.java
+++ rqueue-spring-boot-starter/src/test/java/com/github/sonus21/rqueue/spring/boot/tests/integration/PeriodicMessageTest.java
@@ -26,6 +26,7 @@ import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
@@ -74,6 +75,7 @@ public class PeriodicMessageTest extends SpringTestBase {
   }
 
   @Test
+  @DisabledIfEnvironmentVariable(named = "CI", matches = "true")
   public void testPeriodicMessageMilliseconds() throws TimedOutException {
     PeriodicJob job = PeriodicJob.newInstance();
     String messageId = rqueueMessageEnqueuer.enqueuePeriodic(periodicJobQueue, job, 2000);
