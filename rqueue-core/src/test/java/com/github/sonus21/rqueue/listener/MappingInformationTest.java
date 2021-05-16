0b84025a79c8da94354a5a1dafee53f9a65f492a
Sonu Kumar <sonunitw12@gmail.com>
2021-05-16 18:20:49 +0530

    updated test

--- rqueue-core/src/test/java/com/github/sonus21/rqueue/listener/MappingInformationTest.java
+++ rqueue-core/src/test/java/com/github/sonus21/rqueue/listener/MappingInformationTest.java
@@ -28,10 +28,13 @@ import org.junit.jupiter.api.Test;
 @CoreUnitTest
 class MappingInformationTest extends TestBase {
   @Test
-  void builderToString() {
-    MappingInformation.MappingInformationBuilder builder =
-        MappingInformation.builder().queueNames(Collections.singleton("test-queue")).active(true);
-    System.out.println(builder.toString());
+  void testToString() {
+    MappingInformation mappingInformation =
+        MappingInformation.builder()
+            .queueNames(Collections.singleton("test-queue"))
+            .active(true)
+            .build();
+    assertEquals("test-queue", mappingInformation.toString());
   }
 
   @Test
