af4b9fa72a575826d075e2331e93c7c8e7816138
Sonu Kumar <sonunitw12@gmail.com>
2022-12-24 16:04:09 +0530

    add gradle retry plugin

--- rqueue-core/src/main/java/com/github/sonus21/rqueue/utils/StackTraceUtil.java
+++ rqueue-core/src/main/java/com/github/sonus21/rqueue/utils/StackTraceUtil.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class StackTraceUtil {
+public final class StackTraceUtil {
 
   public void printStackTrace(int frameCount) {
     Thread thread = Thread.currentThread();
--- test.sh
+++ test.sh
@@ -1,14 +1,17 @@
+#!/bin/zsh
+function runTests(){
+  ./gradlew codeCoverageReport -DincludeTags=unit
+  ./gradlew codeCoverageReport -DincludeTags=producerOnly
+  ./gradlew codeCoverageReport -DincludeTags=integration -DexcludeTags=redisCluster,producerOnly,local
+  ./gradlew codeCoverageReport -DincludeTags=redisCluster
+  ./gradlew codeCoverageReport -DincludeTags=local
 
-./gradlew codeCoverageReport -DincludeTags=unit
-./gradlew codeCoverageReport -DincludeTags=producerOnly
-./gradlew codeCoverageReport -DincludeTags=integration -DexcludeTags=redisCluster,producerOnly,local
-./gradlew codeCoverageReport -DincludeTags=redisCluster
-./gradlew codeCoverageReport -DincludeTags=local
+}
+echo "Starting to run tests  $(date)"
+runTests
+echo "Ended run tests  $(date)"
 
 export RQUEUE_REACTIVE_ENABLED=true
-
-./gradlew codeCoverageReport -DincludeTags=unit
-./gradlew codeCoverageReport -DincludeTags=producerOnly
-./gradlew codeCoverageReport -DincludeTags=integration -DexcludeTags=redisCluster,producerOnly,local
-./gradlew codeCoverageReport -DincludeTags=redisCluster
-./gradlew codeCoverageReport -DincludeTags=local
+echo "Starting to run reactive redis tests  $(date)"
+runTests
+echo "ended reactive redis tests  $(date)"
